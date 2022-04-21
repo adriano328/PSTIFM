74c90d6858cedf615e19ca335835e277453c6d0a
aleferamos <alefepdias@gmail.com>
2022-04-21 17:20:53 -0400

    Alteração

--- lib/home/profileForm.dart
+++ lib/home/profileForm.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_new, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/formUserProfessional/formUsersProfessional.dart';
 import 'package:flutter_application_1/formUsers.dart';
@@ -11,31 +13,50 @@ class profileForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <
-              Widget>[
-            const SizedBox(
-              height: 780,
-            ),
-            TextButton(
-                onPressed: () => {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => formUsers()))
-                    },
-                child:
-                    Image.asset('assets/cliente_icone.png', fit: BoxFit.cover)),
-            TextButton(
-                onPressed: () => {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => formUsersProfessional()))
-                    },
-                child: Image.asset('assets/profissional_icone.png',
-                    fit: BoxFit.cover))
-          ]),
-        ),
-      ),
+          body: Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: Center(
+                child: GridView(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    shrinkWrap: true,
+                    // Create a grid with 2 columns. If you change the scrollDirection to
+                    // horizontal, this produces 2 rows.
+
+                    // Generate 100 widgets that display their index in the List.
+
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15.0),
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: AssetImage('assets/cliente_icone.png'))),
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Text('Nouveautés'),
+                        ),
+                      ),
+                      Container(
+                        child: Container(
+                          height: 300,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15.0),
+                              image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image:
+                                      AssetImage('assets/cliente_icone.png'))),
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Text('Nouveautés'),
+                          ),
+                        ),
+                      )
+                    ]),
+              ))),
     );
   }
 }
