58be8dc9437f71aabb14f9ec0e368ea7ccb98d7d
aleferamos <alefepdias@gmail.com>
2022-04-21 20:44:23 -0400

    ajustando rotas

--- lib/home/profileForm.dart
+++ lib/home/profileForm.dart
@@ -1,10 +1,20 @@
 // ignore_for_file: unnecessary_new, prefer_const_constructors
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/formUserProfessional/formUsersProfessional.dart';
 import 'package:flutter_application_1/formUsers.dart';
 
 void main() => runApp(profileForm());
+var listTitle = ['Cliente', 'Profissional'];
+var listImgs = [
+  'assets/cliente_icone.png',
+  'assets/profissional_icone.png',
+  'assets/profissional_icone.png'
+];
+
+var listRouter = [formUsers(), formUsersProfessional(), formUsers()];
 
 class profileForm extends StatelessWidget {
   const profileForm({Key? key}) : super(key: key);
@@ -12,68 +22,40 @@ class profileForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          body: Container(
-              margin: const EdgeInsets.only(top: 10),
-              child: Center(
-                child: GridView(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                    ),
-                    shrinkWrap: true,
-                    // Create a grid with 2 columns. If you change the scrollDirection to
-                    // horizontal, this produces 2 rows.
-
-                    // Generate 100 widgets that display their index in the List.
-
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => formUsers()));
-                        },
-                        child: Container(
-                          height: 300,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15.0),
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image:
-                                      AssetImage('assets/cliente_icone.png'))),
-                          child: Center(
-                            child: Text('Profissional'),
-                          ),
-                        ),
-                      ),
-                      InkWell(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      formUsersProfessional()));
-                        },
-                        child: Container(
-                          child: Container(
-                            height: 300,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15.0),
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: AssetImage(
-                                        'assets/profissional_icone.png'))),
-                            child: Center(
-                              child: Text('Profissional'),
-                            ),
-                          ),
-                        ),
-                      )
-                    ]),
-              ))),
-    );
+        home: Scaffold(
+      body: Container(
+          margin: const EdgeInsets.only(top: 280),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              mainAxisExtent: 170, // here set custom Height You Want
+            ),
+            itemCount: 2,
+            itemBuilder: (BuildContext context, int index) {
+              return InkWell(
+                onTap: () => {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => listRouter[index]))
+                },
+                child: Container(
+                  height: 300,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15.0),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(listImgs[index]))),
+                  child: Align(
+                      child: Text(listTitle[index]),
+                      alignment: Alignment(0, 1.3)),
+                ),
+              );
+            },
+          )),
+    ));
   }
 }
